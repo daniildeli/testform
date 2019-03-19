a7ed1a86e54bf8920718a57357a7d452a046cbe8
Danyil Deli <danyil.deli@globallogic.com>
2019-03-19 14:52:12 +0200

    Changed data in db

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-let login = "login";
-let pass = 'pass';
+// let login = "login";
+// let pass = 'pass';
 
-let xhr = new XMLHttpRequest();
+// let xhr = new XMLHttpRequest();
 
-let body = {
-  "title": "titleName",
-  "body": 'Body'
-};
+// let body = {
+//   "title": "titleName",
+//   "body": 'Body'
+// };
 
-xhr.open('POST', 'https://my-json-server.typicode.com/daniildeli/testform/posts', true);
-// xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-xhr.setRequestHeader('Content-Type', 'application/json');
+// xhr.open('POST', 'https://my-json-server.typicode.com/daniildeli/testform/posts', true);
+// // xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+// xhr.setRequestHeader('Content-Type', 'application/json');
 
-xhr.send(JSON.stringify(body));
+// xhr.send(JSON.stringify(body));
 
-xhr.onreadystatechange = function () {
-  console.log(xhr.responseText);
-};
+// xhr.onreadystatechange = function () {
+//   console.log(xhr.responseText);
+// };
